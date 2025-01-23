3413ef7e1c8aae9dbac276beffb60aad0def7775
Nagambika06 <nagambikadevadiga@gmail.com>
2025-01-23 13:38:53 +0530

    Add count feature

--- Dice game/main.py
+++ Dice game/main.py
@@ -1,13 +1,16 @@
 import random
 
+count=0
 while True:
     choice=input("Are you ready to play (Y/N): ").lower()
     if(choice=='y'):
         die1=random.randint(1,6)
         die2=random.randint(1,6)
         print(f"({die1}, {die2})")
+        count+=1
     elif(choice=='n'):
         print("Thanks for playing!!")
+        print(f'You have rolled dice {count} Times')
         break
     else:
         print("Invalid choice")
