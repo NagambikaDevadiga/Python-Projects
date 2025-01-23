b386b548afeb1c867fa977812ba43e73848faa41
Nagambika06 <nagambikadevadiga@gmail.com>
2025-01-23 11:43:51 +0530

    Number guessing game

---
+++ Number Guessing game/main.py
@@ -0,0 +1,16 @@
+import random
+
+random_num=random.randint(1,100)
+n=-1
+guesses=1
+while(n!=random_num):
+    n=int(input("Guess the number: "))
+    if(n>random_num):
+        print("Lower number please!")
+        guesses+=1
+    elif(n<random_num):
+        print("Higher number please")
+        guesses+=1
+
+print(f"You guessed the number {random_num} in {guesses} attempts!!")
+    
